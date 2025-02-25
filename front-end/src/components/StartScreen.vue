d82573e8a1602e0327d7b7e41e94f8012092f6e9
Thu Ya Kyaw <contact.thuya@gmail.com>
2025-02-26 04:03:08 +0800

    added front-end

---
+++ front-end/src/components/StartScreen.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <h1> Welcome Player! </h1>
+            <h3> Are you ready to experice Gemini 2.0's real time guidance, and see how AI can elevate your gameplay?</h3>
+            <p> Roll a ball to start playing! </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
